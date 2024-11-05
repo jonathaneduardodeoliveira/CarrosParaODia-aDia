dd074eac27e8784bc8f8ef02ec2fef5677409f10
Jonathan Eduardo de Oliveira <jonathan.mega2@gmail.com>
2024-11-05 20:52:02 -0300

    Implementa CategoriaService com métodos para manipulação de categorias

---
+++ src/main/java/diadia/carro/service/CategoriaService.java
@@ -0,0 +1,35 @@
+package diadia.carro.service;
+
+import diadia.carro.model.Categoria;
+import diadia.carro.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoriaService {
+
+    private final CategoriaRepository categoriaRepository;
+
+    @Autowired
+    public CategoriaService(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    public Categoria salvarCategoria(Categoria categoria) {
+        return categoriaRepository.save(categoria);
+    }
+
+    public List<Categoria> listarCategorias() {
+        return categoriaRepository.findAll();
+    }
+
+    public Categoria buscarCategoriaPorId(Long id) {
+        return categoriaRepository.findById(id).orElse(null);
+    }
+
+    public void excluirCategoria(Long id) {
+        categoriaRepository.deleteById(id);
+    }
+}
