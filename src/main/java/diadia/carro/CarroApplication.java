1ca0e94e5bbe277edb8d7b0755be68aa79f3a1f4
Jonathan Eduardo de Oliveira <jonathan.mega2@gmail.com>
2024-11-05 21:14:46 -0300

    Configura classe principal CarroApplication para inicializar aplicação Spring Boot

--- src/main/java/diadia/carro/CarroApplication.java
+++ src/main/java/diadia/carro/CarroApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarroApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarroApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CarroApplication.class, args);
+    }
 }
